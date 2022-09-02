cb3d74f44da2948fdc535e34e240109da089ef7c
Pedro Henrique Botelho <54961750+pedrobotelhot@users.noreply.github.com>
2022-09-02 17:49:56 -0300

    Add files via upload

---
+++ repository-project/src/main/java/br/ufpb/dcx/dsc/repositoryproject/RepositoryProjectApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.dsc.repositoryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryProjectApplication.class, args);
+	}
+
+}
